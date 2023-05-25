abceef7da22ac8fb26b9ea70a65534bc9bd58617
PratikUp19 <84855152+PratikUp19@users.noreply.github.com>
2023-05-25 16:47:46 +0530

    Final Commit

--- client/src/pages/Logistic/Profile.js
+++ client/src/pages/Logistic/Profile.js
@@ -98,6 +98,7 @@ import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import LogisticForm from "../../components/LogisticForm";
 import moment from "moment";
+import "./Profile.css";
 
 function Profile() {
   const { user } = useSelector((state) => state.user);
@@ -167,9 +168,10 @@ function Profile() {
   }, []);
   return (
     <Layout>
-      <h1 className="page-title">Logistic Profile</h1>
+      <h1 className="page-title" style={{ textAlign: 'center' }}>Logistic Profile</h1>
       <hr />
-      {logistic && <LogisticForm onFinish={onFinish} initivalValues={logistic} />}
+      <div className="centered-form">
+      {logistic && <LogisticForm onFinish={onFinish} initivalValues={logistic} />}</div>
     </Layout>
   );
 }
